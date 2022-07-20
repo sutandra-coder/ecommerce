810425f2b25f726b07f8cbcc9866fb92c075f4dd
sutandra-coder <72545443+sutandra-coder@users.noreply.github.com>
2022-07-20 20:03:22 +0530

    u
    u

---
+++ awsconfig.py
@@ -0,0 +1,9 @@
+# AKIAQ2AM5BTXOGOOFDOA
+
+# Tpu74vrig6jElXPzXFWeki0hWpXUFLJjznPSTFpU
+
+
+import os
+ACCESS_KEY =  'AKIAQ2AM5BTXOGOOFDOA'
+SECRET_KEY = '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'
+S3_LOCATION = 'http://{}.s3.amazonaws.com/'.format('myelsapython')
