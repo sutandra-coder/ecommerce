186cc3bf4469ba3ce56015783c098da0ef97a2d5
sutandra-coder <72545443+sutandra-coder@users.noreply.github.com>
2022-07-20 13:50:37 +0530

    Update ecommerce_organisation_otp.py
    Update ecommerce_organisation_otp.py

--- ecommerce_organisation_otp.py
+++ ecommerce_organisation_otp.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify, json
 from flask_api import status
-from jinja2._compat import izip
 from jinja2 import Environment, FileSystemLoader
 from datetime import datetime,timedelta,date
 import pymysql
