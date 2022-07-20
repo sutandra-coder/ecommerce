f686bb47d6f242df9369accc8b3eebba2ef40022
sutandra-coder <72545443+sutandra-coder@users.noreply.github.com>
2022-07-20 20:06:53 +0530

    Add confiig
    Add confiig

---
+++ awsconfige.py
@@ -0,0 +1,9 @@
+# AKIAQ2AM5BTXOGOOFDOA
+
+# Tpu74vrig6jElXPzXFWeki0hWpXUFLJjznPSTFpU
+
+
+import os
+ACCESS_KEY =  'AKIAQ2AM5BTXLLVHT4WS'
+SECRET_KEY = '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'
+S3_LOCATION = 'http://{}.s3.amazonaws.com/'.format('myelsapython')
